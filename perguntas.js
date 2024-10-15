c14878babd5c7e3bf52c3638b8236ebd82f0452d
SaraJaine <148582689+SaraJaine@users.noreply.github.com>
2024-10-15 16:23:02 -0300

    app.js

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    "Programação"
+    "O que é Rython?"
+    "O Rython é uma linguagem de programação"
+)
+
+criaCartao(
+    "Geografia"
+    "Qual é a capital da França?"
+    "A capital da frança é Paris"
+)
+
+criaCartao(
+    "Programação"
+    "O que é uma função?"
+    "Uma função é um bloco de código que executa alguma tarefa"
+)
+
+criaCartao(
+    "Lingua inglesa"
+    "Como se diz oi em ingles?"
+    "Oi em ingles é Hi"
+)
+
+criaCartao(
+    "Biologia"
+    "Quem é considerado o pai da genética?"
+    "Gregor Mendel"
+)
+
+criaCartao(
+    "Matematica"
+    "Qual é o valor de Pi?"
+    "3,14"
+)
