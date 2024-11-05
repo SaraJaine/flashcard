a77c8ec7b46ee05a0325511ec44e2c6a3841b0c1
SaraJaine <148582689+SaraJaine@users.noreply.github.com>
2024-11-05 16:09:36 -0300

    perguntas

--- perguntas.js
+++ perguntas.js
@@ -1,35 +1,35 @@
 criaCartao(
-    'Biologia',
-    'Qual é a principal diferença entre a primeira e a segunda lei de Mendel?',
-    'Na primeira verifica-se a expressão de apenas uma característica (monoibridismo), na segunda duas ou mais (diibridísmo)'
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
 )
- 
-criaCartao(
-    'Biologia',
-    'A Segunda Lei de Mendel é sempre obedecida no processo de produção de características físicas?',
-    'Não! Quando os genes estão presentes no mesmo cromossomo ocorre por Linkage'
-)
- 
-criaCartao(
-    'Biologia',
-    'As síndromes de Down, Turner, Patau, Edwards e de Klinefelter são mutações conhecidas por?',
-    'aneuploidias'
-)
- 
+
 criaCartao(
     'Geografia',
-    'A Caatinga ocupa uma grande área da região Nordeste do país. Sobre as principais características do bioma é INCORRETO afirmar que?',
-    'O bioma passa por curtos períodos de seca e, por isso, sua vegetação perde as folhas, os galhos tornam-se esbranquiçados e muitas plantas apresentam espinhos;'
+    'Qual a capital da França?',
+    'A capital da França é Paris'
 )
- 
+
 criaCartao(
-    'Geografia',
-    'As ilhas de calor representam um dos problemas ambientais urbanos. Esse fenômeno climático acontece devido',
-    'a elevação das temperaturas em algumas zonas urbanas.
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
 )
- 
+
 criaCartao(
-    'Histódria',
-    'Qual foi o médico envolvido na questão da vacinação obrigatória na Revolta da Vacina?',
-    'Oswaldo Cruz'
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Biologia',
+    'Quem é considerado o pai da genética?',
+    'Gregor Mendel'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é o valor de Pi?',
+    '3,14'
 )
