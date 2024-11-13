5c227415ed091c25629c97f87fa7ec2e1a68e576
SaraJaine <148582689+SaraJaine@users.noreply.github.com>
2024-11-13 09:50:49 -0300

    bhubiog

--- perguntas.js
+++ perguntas.js
@@ -21,13 +21,33 @@ criaCartao (
     'Quantas casas decimais tem o número pi?',
     'Infinitas'
 )
+
 criaCartao(
     'Física',
     'Qual a velocidade da luz?',
     '299 792 458 metros por segundo (m/s)'
 )
+
 criaCartao(
     'Artes',
     'Quem pintou "Guernica"?',
     'Pablo Picasso'
+)
+
+criaCartao(
+    'Filosofia',
+    'De quem é a famosa frase “Penso, logo existo”?',
+    'Descartes'
+)
+
+criaCartao(
+    'Biologia',
+    'Pares de genes, com segregação independente podem agir, conjuntamente, na determinação de uma mesma característica fenotípica. Este fenômeno é conhecido como:',
+    'interação gênica'
+)
+
+criaCartao(
+    'Biologia',
+    'Uma característica genética recessiva presente no cromossomo Y é:',
+    'só poder ser herdada a partir do pai por seus descendentes do sexo masculino.'
 )
